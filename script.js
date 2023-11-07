3c678b952420e4faad43706a30650f1b330277d0
neilronjon <150090943+neilronjon@users.noreply.github.com>
2023-11-06 19:18:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*document.getElementById("rangeEstimatorForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    let batteryCapacity = document.getElementById("batteryCapacity").value;
+    let consumptionRate = document.getElementById("consumptionRate").value;
+
+    let estimatedRange = batteryCapacity * 1000 / consumptionRate;
+
+    document.getElementById("outputRange").textContent = estimatedRange.toFixed(2) + " miles";
+});*/
